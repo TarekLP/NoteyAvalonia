170245d6654f67f0e4ff87900dabb16b0bbd72e2
TarekInnit <76664991+TarekLP@users.noreply.github.com>
2026-03-29 11:34:36 +0200

    Everything works for once!

--- NoteyAvalonia/Views/BoardView.axaml.cs
+++ NoteyAvalonia/Views/BoardView.axaml.cs
@@ -1,74 +1,96 @@
-﻿using Avalonia.Controls;
+﻿using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Input;
 using NoteToolAvalonia.Models;
 using NoteToolAvalonia.ViewModels;
+using System;
 
 namespace NoteToolAvalonia.Views;
 
 public partial class BoardView : UserControl
 {
-	private static NoteCard? _draggedCard;
-	private bool _isDragging;
+	private Point _pressPosition;
+	private bool _isPressed;
+	private NoteCard? _pendingDragCard;
 
 	public BoardView()
 	{
 		InitializeComponent();
 	}
 
-	private async void NoteCard_PointerPressed(object? sender, PointerPressedEventArgs e)
+	private void NoteCard_PointerPressed(object? sender, PointerPressedEventArgs e)
 	{
-		if (!e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
-			return;
+		if (!e.GetCurrentPoint(this).Properties.IsLeftButtonPressed) return;
 
 		if (sender is Control control && control.DataContext is NoteCard card)
 		{
-			_isDragging = false;
-			_draggedCard = card;
+			_isPressed = true;
+			_pressPosition = e.GetPosition(this);
+			_pendingDragCard = card;
+		}
+	}
 
-			var result = await DragDrop.DoDragDropAsync(e, new DataTransfer(), DragDropEffects.Move);
+	private void NoteCard_PointerMoved(object? sender, PointerEventArgs e)
+	{
+		if (!_isPressed || _pendingDragCard == null) return;
 
-			if (result != DragDropEffects.None)
-			{
-				_isDragging = true;
-			}
+		var currentPos = e.GetPosition(this);
+		var distance = Math.Sqrt(
+			Math.Pow(currentPos.X - _pressPosition.X, 2) +
+			Math.Pow(currentPos.Y - _pressPosition.Y, 2));
+
+		if (distance > 5)
+		{
+			_isPressed = false;
+			var draggedCard = _pendingDragCard;
+			_pendingDragCard = null;
+
+			// ✅ WORKAROUND: Use DataObject + synchronous DoDragDrop
+			var data = new DataObject();
+			data.Set("NoteCard", draggedCard);
+
+			DragDrop.DoDragDrop(e, data, DragDropEffects.Move);
+		}
+	}
+
+	private void NoteCard_PointerReleased(object? sender, PointerReleasedEventArgs e)
+	{
+		_isPressed = false;
+		_pendingDragCard = null;
+	}
+
+	private void NoteCard_DoubleTapped(object? sender, TappedEventArgs e)
+	{
+		if (sender is Control control &&
+			control.DataContext is NoteCard card &&
+			DataContext is BoardViewModel viewModel)
+		{
+			_isPressed = false;
+			_pendingDragCard = null;
+			viewModel.OpenCardCommand.Execute(card);
+			e.Handled = true;
 		}
 	}
 
 	private void Column_DragOver(object? sender, DragEventArgs e)
 	{
-		e.DragEffects = _draggedCard != null
-			? DragDropEffects.Move
-			: DragDropEffects.None;
+		if (e.Data.Contains("NoteCard"))
+			e.DragEffects = DragDropEffects.Move;
+		else
+			e.DragEffects = DragDropEffects.None;
 	}
 
 	private void Column_Drop(object? sender, DragEventArgs e)
 	{
-		if (_draggedCard == null) return;
+		if (!e.Data.Contains("NoteCard") ||
+			e.Data.Get("NoteCard") is not NoteCard draggedCard)
+			return;
 
 		if (sender is Control control &&
 			control.DataContext is BoardColumn targetColumn &&
 			DataContext is BoardViewModel viewModel)
 		{
-			viewModel.MoveCardToColumn(_draggedCard, targetColumn);
-			_isDragging = true;
-		}
-
-		_draggedCard = null;
-	}
-
-	private void NoteCard_DoubleTapped(object? sender, TappedEventArgs e)
-	{
-		if (_isDragging)
-		{
-			_isDragging = false;
-			return;
-		}
-
-		if (sender is Control control &&
-			control.DataContext is NoteCard card &&
-			DataContext is BoardViewModel viewModel)
-		{
-			viewModel.OpenCardCommand.Execute(card);
+			viewModel.MoveCardToColumn(draggedCard, targetColumn);
 		}
 	}
 }
